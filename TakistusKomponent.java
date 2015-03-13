839635edf46276c4480ec3ebd958ded3c16acf8a
ophyneus <ophyneus@tlu.ee>
2015-03-13 11:07:47 +0200

    Takisti klass koos prooviga

--- TakistusKomponent.java
+++ TakistusKomponent.java
@@ -2,6 +2,7 @@ abstract class TakistusKomponent{
 	abstract double kysiTakistus();
 	/*
 	* Leitakse vool vastavalt pingele
+	* @param pinge - rakendatav pinge voltides
 	*/
 	public double vool(double pinge){
 		return pinge/kysiTakistus();
