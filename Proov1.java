3e53710d655c69cda8e3ae408715eef5ae247c4a
ophyneus <ophyneus@tlu.ee>
2015-03-13 11:44:43 +0200

    Koos laadija ja tarbijaga

--- Proov1.java
+++ Proov1.java
@@ -3,5 +3,8 @@ public class Proov1{
 		TakistusKomponent t=new Takisti220oomi();
 		System.out.println (t.kysiTakistus());
 		System.out.println ("Vool 9 korral: "+t.vool(9));
+		TakistusKomponent t2=new Takisti(500);
+		System.out.println (t2.kysiTakistus());
+		System.out.println ("Vool 9 korral: "+t2.vool(9));
 	}
 }
--- Takisti220oomi.java
+++ Takisti220oomi.java
@@ -3,3 +3,6 @@ public class Takisti220oomi extends TakistusKomponent{
 		return 220;
 	}
 }
+
+//luua klass Takisti, mille konstruktori parameetrina antakse tema takistus oomides
+//katsetada Takistit proovifailis
