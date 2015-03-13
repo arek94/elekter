e5d955a9c328a452f89151781b4cd593fe44486e
ophyneus <ophyneus@tlu.ee>
2015-03-13 10:57:54 +0200

    Pinge parameetri kommentaar

--- TekistusKomponent.java
+++ TekistusKomponent.java
@@ -1,5 +1,8 @@
 abstract class TakistusKomponent{
 	abstract double kysiTakistus();
+	/*
+	* Leitakse vool vastavalt pingele
+	*/
 	public double vool(double pinge){
 		return pinge/kysiTakistus();
 	}
